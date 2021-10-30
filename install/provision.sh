5cbd9b24e38147fc64bf1d6a1aaaf7e71086639d
Jason Spriggs <jason@jasonspriggs.com>
2021-10-30 10:59:13 -0400

    Cleanup before PR

--- install/provision.sh
+++ install/provision.sh
@@ -3,4 +3,4 @@
 export AWS_ACCESS_KEY_ID=
 export AWS_SECRET_ACCESS_KEY=
 export AWS_DEFAULT_REGION=us-east-1
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/magfest/mindbeamer/main/install/client.sh)"
+sudo -E java -Droot="/greengrass/v2" -Dlog.store=FILE -jar /opt/greengrass/lib/Greengrass.jar --aws-region us-east-1 --thing-name Mindbeamer-$(cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2) --thing-group-name MindbeamerDisplayGroup --component-default-user ggc_user:ggc_group --provision true --setup-system-service true --deploy-dev-tools true
