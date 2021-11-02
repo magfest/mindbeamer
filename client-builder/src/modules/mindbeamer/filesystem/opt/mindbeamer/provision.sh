b4d96d98b650e85d4f7933a5d2ac3f259a75027c
Jason Spriggs <jason@jasonspriggs.com>
2021-11-02 17:28:28 -0400

    Add auto recog of USB devices

---
+++ client-builder/src/modules/mindbeamer/filesystem/opt/mindbeamer/provision.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# If this is a new install of Mindbeamer...
+if [ ! -f /opt/mindbeamer/provisioned ]; then
+    # If the USB drive is plugged in and has a provision.sh file...
+    if [ -f /media/usb/provision.sh ]; then
+    # Check to see if the file is signed by a key we trust
+        gpg --no-default-keyring --keyring /home/pi/.gnupg/pubring.kbx --verify /media/usb/provision.sh.sig /media/usb/provision.sh
+
+        if [ $? -eq 0 ]; then
+            # If it does, run the provision.sh file
+            /bin/bash /media/usb/provision.sh
+            # And then mark this pi as provisioned
+            touch /opt/mindbeamer/provisioned
+            # Move the splash screen to be provisioned
+            rm /boot/splash.png
+            mv /boot/splash-ready.png /boot/splash.png
+        else
+            echo Signature is not valid.
+        fi
+    fi
+fi
