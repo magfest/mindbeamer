e3b246cb3f292210e924d8f3598463d0787d439f
Jason Spriggs <jason@jasonspriggs.com>
2021-11-03 18:18:59 -0400

    Use FPDashboard, update script

--- client-builder/src/modules/mindbeamer/filesystem/opt/mindbeamer/node-splash.sh
+++ client-builder/src/modules/mindbeamer/filesystem/opt/mindbeamer/node-splash.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # If the node server is not listening on port 3000, show the splash screen
-$port="3000"
-if [[ $(netstat -ltn | grep ":${port} " | wc -l) -eq "0" ]] ; then 
-  echo "http://localhost/waiting.html" > /boot/fullpageos.txt
+if [[ $(netstat -ltn | grep ":3000" | wc -l) -eq "0" ]] ; then 
+  echo "http://localhost/waiting.html" > /boot/fullpagedashboard.txt
   /bin/bash /home/pi/scripts/refresh
 else
-  echo "http://localhost:3000" > /boot/fullpageos.txt
+# If it is listening, switch to the node port
+  echo "http://localhost:3000" > /boot/fullpagedashboard.txt
   /bin/bash /home/pi/scripts/refresh
 fi
-# If it is listening, switch to the node port
