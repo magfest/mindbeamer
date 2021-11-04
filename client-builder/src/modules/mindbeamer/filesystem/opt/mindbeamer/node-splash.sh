ee2afaf1d02430d38b665e88fe75dffe848fb01a
Jason Spriggs <jason@jasonspriggs.com>
2021-11-03 22:31:04 -0400

    Have pi user run refresh

--- client-builder/src/modules/mindbeamer/filesystem/opt/mindbeamer/node-splash.sh
+++ client-builder/src/modules/mindbeamer/filesystem/opt/mindbeamer/node-splash.sh
@@ -5,13 +5,11 @@ if [[ $(netstat -ltn | grep ":3000" | wc -l) -eq "0" ]] ; then
   tmp=$(mktemp)
   jq '.urls[0].url = "http:\/\/localhost\/waiting.html"' /var/www/html/FullPageDashboard/urls.json > "$tmp" && mv "$tmp" /var/www/html/FullPageDashboard/urls.json
   chown www-data:www-data /var/www/html/FullPageDashboard/urls.json
-  /bin/bash /home/pi/scripts/refresh
   rm "$tmp"
 else
 # If it is listening, switch to the node port
   tmp=$(mktemp)
   jq '.urls[0].url = "http:\/\/localhost:3000"' /var/www/html/FullPageDashboard/urls.json > "$tmp" && mv "$tmp" /var/www/html/FullPageDashboard/urls.json
   chown www-data:www-data /var/www/html/FullPageDashboard/urls.json
-  /bin/bash /home/pi/scripts/refresh
   rm "$tmp"
 fi
