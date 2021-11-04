7ddbd76948f9505d6a63c9f8fe64442c07fc0f8c
Jason Spriggs <jason@jasonspriggs.com>
2021-11-03 21:06:47 -0400

    Use JQ to modify config

--- client-builder/src/modules/mindbeamer/filesystem/opt/mindbeamer/node-splash.sh
+++ client-builder/src/modules/mindbeamer/filesystem/opt/mindbeamer/node-splash.sh
@@ -2,10 +2,16 @@
 
 # If the node server is not listening on port 3000, show the splash screen
 if [[ $(netstat -ltn | grep ":3000" | wc -l) -eq "0" ]] ; then 
-  echo "http://localhost/waiting.html" > /boot/fullpagedashboard.txt
+  tmp=$(mktemp)
+  jq '.urls[0].url = "http:\/\/localhost\/waiting.html"' /var/www/html/FullPageDashboard/urls.json > "$tmp" && mv "$tmp" /var/www/html/FullPageDashboard/urls.json
+  chown www-data:www-data /var/www/html/FullPageDashboard/urls.json
   /bin/bash /home/pi/scripts/refresh
+  rm "$tmp"
 else
 # If it is listening, switch to the node port
-  echo "http://localhost:3000" > /boot/fullpagedashboard.txt
+  tmp=$(mktemp)
+  jq '.urls[0].url = "http:\/\/localhost:3000"' /var/www/html/FullPageDashboard/urls.json > "$tmp" && mv "$tmp" /var/www/html/FullPageDashboard/urls.json
+  chown www-data:www-data /var/www/html/FullPageDashboard/urls.json
   /bin/bash /home/pi/scripts/refresh
+  rm "$tmp"
 fi
