3ce6be986ad38cd031b5ea74beda30d1cfe67059
Jason Spriggs <jason@jasonspriggs.com>
2021-11-06 16:53:32 -0400

    Fix wpa-supplicant script

--- client-builder/src/modules/mindbeamer/filesystem/opt/mindbeamer/wpa-supplicant.sh
+++ client-builder/src/modules/mindbeamer/filesystem/opt/mindbeamer/wpa-supplicant.sh
@@ -6,8 +6,18 @@ if [ -f /media/usb/mindbeamer-wpa-supplicant.txt ]; then
     gpg --no-default-keyring --keyring /opt/mindbeamer/signer.kbx --verify /media/usb/mindbeamer-wpa-supplicant.txt.sig /media/usb/mindbeamer-wpa-supplicant.txt
 
     if [ $? -eq 0 ]; then
-        # If it does, copy the new config over
-        cp /media/usb/mindbeamer-wpa-supplicant.txt /boot/mindbeamer-wpa-supplicant.txt
+        diff /media/usb/mindbeamer-wpa-supplicant.txt /boot/mindbeamer-wpa-supplicant.txt
+
+        # If there's new content
+        if [ ! $? -q 0 ]; then
+            # If it does, copy the new config over
+            cp /media/usb/mindbeamer-wpa-supplicant.txt /boot/mindbeamer-wpa-supplicant.txt
+
+            # And then reboot this bad boy
+            reboot
+        else 
+            echo No new WiFi config to update.
+        fi
     else
         echo WPA Supplicant signature is not valid. Not copying.
     fi
