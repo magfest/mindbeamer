100495442d85799262bd454dbf1b99b212c5c60d
Jason Spriggs <jason@jasonspriggs.com>
2021-12-16 23:18:45 -0500

    Update node-splash to reduce refreshes

--- client-builder/README.md
+++ client-builder/README.md
@@ -6,5 +6,6 @@
 1. Update the `mindbeamer-wpa-supplicant.txt` file with your network details. Remember Pis only have 2.4Ghz networking, so use that network.
 1. Add AWS credentials w/ AdministratorAccess to your `provision.sh` file.
 1. On a box with Keybase installed, run `sign.sh` to sign the two files with your PGP key.
-1. Plug the SD card AND the USB stick into the pi, power it on, and wait for roughly 25 minutes. The Pi will reboot a number of times during this process.
+1. Plug the SD card AND the USB stick into the pi, power it on, and wait for roughly 25 minutes. The Pi will reboot a number of times during this process. 
+1. Once the screen says "We'll be right back" instead of "Ready for Setup", you can remove the USB stick. (normally around 4 minutes after first boot)
 1. Done!
--- client-builder/src/modules/mindbeamer/filesystem/opt/mindbeamer/node-splash.sh
+++ client-builder/src/modules/mindbeamer/filesystem/opt/mindbeamer/node-splash.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+fpdconfig=/var/www/html/FullPageDashboard/urls.json
+currentsum=$(md5sum "$fpdconfig" | cut -d ' ' -f 1)
+tmp=$(mktemp)
+
 # If the node server is not listening on port 3000, show the splash screen
 if [[ $(netstat -ltn | grep ":3000" | wc -l) -eq "0" ]] ; then 
-  tmp=$(mktemp)
-  jq '.urls[0].url = "http:\/\/localhost\/waiting.html"' /var/www/html/FullPageDashboard/urls.json > "$tmp" && mv "$tmp" /var/www/html/FullPageDashboard/urls.json
-  chown www-data:www-data /var/www/html/FullPageDashboard/urls.json
-  rm "$tmp"
+  jq '.urls[0].url = "http:\/\/localhost\/waiting.html"' "$fpdconfig" > "$tmp"
 else
 # If it is listening, switch to the node port
-  tmp=$(mktemp)
-  jq '.urls[0].url = "http:\/\/localhost:3000"' /var/www/html/FullPageDashboard/urls.json > "$tmp" && mv "$tmp" /var/www/html/FullPageDashboard/urls.json
-  chown www-data:www-data /var/www/html/FullPageDashboard/urls.json
-  rm "$tmp"
+  jq '.urls[0].url = "http:\/\/localhost:3000"' "$fpdconfig" > "$tmp"
+fi
+
+tmpsum=$(md5sum "$tmp" | cut -d ' ' -f 1)
+
+if [ "$currentsum" != "$tmpsum" ] ; then
+  chown www-data:www-data "$tmp"
+  mv "$tmp" "$fpdconfig"
+  /bin/su -c "/home/pi/scripts/refresh" - pi
 fi
