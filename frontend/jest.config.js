d4afe08796abdcd4c574fa0b0141b83f4197f9bb
Valeria Green <raspberryfruitt@gmail.com>
2021-12-31 19:25:09 -0500

    Excluded Coverage from Files & Added Test for App.js * Excluded index.js, reportWebVitals.js, and setupTests.js from the code coverage report due to their irrelevance * Added a React import at the top of App.js * Added a test file for App.js called App.spec.js to just look and see if the component renders

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,7 +3,11 @@ module.exports = {
     clearMocks: true,
   
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    collectCoverageFrom: ['src/**/*.{js,jsx,mjs}'],
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,mjs}',
+        '!src/index.js',
+        '!src/reportWebVitals.js',
+        '!src/setupTests.js'],
   
     // The directory where Jest should output its coverage files
     coverageDirectory: 'coverage',
